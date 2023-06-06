0227718ca547274045e2ed9e2d1dde8611d9a652
ThePhoenix152 <86343466+ThePhoenix152@users.noreply.github.com>
2023-06-06 23:56:59 +0700

    add cors

--- CookingRecipe/Cookapp_API/Program.cs
+++ CookingRecipe/Cookapp_API/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 // Add services to the container.
 
@@ -10,7 +11,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000");
+                      });
+});
 
 builder.Services.AddDbContext<CookingRecipeDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("CookappDB")));
@@ -25,6 +33,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
+
 
 app.UseAuthorization();
 
